e095dbd5a8df6d407378c49dbddd68fd1ecf1961
Shadowfacts <akorde@icloud.com>
2014-10-10 21:00:41 -0400

    Add experience drop to ores

--- src/main/java/net/shadowfacts/zcraft/block/BlockOre.java
+++ src/main/java/net/shadowfacts/zcraft/block/BlockOre.java
@@ -4,6 +4,8 @@ import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.item.EntityXPOrb;
+import net.minecraft.world.World;
 import net.shadowfacts.zcraft.item.ZItems;
 
 public class BlockOre extends Block {
@@ -12,7 +14,7 @@ public class BlockOre extends Block {
 		super(par1, Material.rock);
 	}
 	
-	
+	@Override
 	public int idDropped(int par1, Random par2Random, int par3) {
 		
 		if (this.blockID == ZBlocks.greenZinchoriumGemOre.blockID) {
@@ -23,11 +25,21 @@ public class BlockOre extends Block {
 		
 	}
 	
+	@Override
 	public int quantityDropped(Random par1Random) {
 		if (this.blockID == ZBlocks.greenZinchoriumGemOre.blockID) {
 			return 3;
 		} else {
 			return 1;
+		}
+	}
+	
+	@Override
+	public void dropXpOnBlockBreak(World world, int par2, int par3, int par4, int par5) {
+		if (!world.isRemote) {
+			int i1 = EntityXPOrb.getXPSplit(par5);
+			par5 -= i1;
+			world.spawnEntityInWorld(new EntityXPOrb(world, (double)par2 + 0.5d, (double)par3 + 0.5d, (double)par4 + 0.5d, 20));
 		}
 	}
 
