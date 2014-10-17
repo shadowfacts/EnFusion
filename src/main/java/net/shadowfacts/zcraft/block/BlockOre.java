81333f24d78f662356e924a73aad0758573ed4bb
Shadowfacts <akorde@icloud.com>
2014-10-16 21:36:23 -0400

    Update BlockOre for 1.7.10

--- src/main/java/net/shadowfacts/zcraft/block/BlockOre.java
+++ src/main/java/net/shadowfacts/zcraft/block/BlockOre.java
@@ -5,29 +5,28 @@ import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.item.EntityXPOrb;
+import net.minecraft.item.Item;
 import net.minecraft.world.World;
 import net.shadowfacts.zcraft.item.ZItems;
 
 public class BlockOre extends Block {
 
-	public BlockOre(int par1) {
-		super(par1, Material.rock);
+	public BlockOre() {
+		super(Material.rock);
 	}
 	
 	@Override
-	public int idDropped(int par1, Random par2Random, int par3) {
-		
-		if (this.blockID == ZBlocks.greenZinchoriumGemOre.blockID) {
-			return ZItems.greenZinchoriumGem.itemID;
+	public Item getItemDropped(int par1, Random par2Random, int par3) {
+		if (this == ZBlocks.greenZinchoriumGemOre) {
+			return ZItems.greenZinchoriumGem;
 		} else {
-			return this.blockID;
+			return Item.getItemFromBlock(this);
 		}
-		
 	}
 	
 	@Override
 	public int quantityDropped(Random par1Random) {
-		if (this.blockID == ZBlocks.greenZinchoriumGemOre.blockID) {
+		if (this == ZBlocks.greenZinchoriumGemOre) {
 			return 3;
 		} else {
 			return 1;
