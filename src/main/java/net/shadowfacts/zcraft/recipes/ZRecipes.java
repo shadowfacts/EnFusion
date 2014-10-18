b8ed22e30dd16604675548c1bdc7e078ba1a9d01
Shadowfacts <akorde@icloud.com>
2014-10-17 20:29:53 -0400

    Update ZRecipes for 1.7.10

--- src/main/java/net/shadowfacts/zcraft/recipes/ZRecipes.java
+++ src/main/java/net/shadowfacts/zcraft/recipes/ZRecipes.java
@@ -1,7 +1,7 @@
 package net.shadowfacts.zcraft.recipes;
 
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 import net.shadowfacts.zcraft.block.ZBlocks;
@@ -18,27 +18,27 @@ public class ZRecipes {
 	
 	private static void registerBlockRecipes() {
 		// Green Zinchorium Light
-		GameRegistry.addShapedRecipe(new ItemStack(ZBlocks.greenZinchoriumLightIdle, 1), "IGI", "GLG", "IGI", 'I', new ItemStack(ZItems.greenZinchoriumGem), 'G', new ItemStack(Block.glass), 'L', new ItemStack(ZItems.lightBulb, 1));
+		GameRegistry.addShapedRecipe(new ItemStack(ZBlocks.greenZinchoriumLightIdle, 1), "IGI", "GLG", "IGI", 'I', new ItemStack(ZItems.greenZinchoriumGem), 'G', new ItemStack(Blocks.glass), 'L', new ItemStack(ZItems.lightBulb, 1));
 	}
 	
 	private static void registerItemRecipes() {
 		// Light bulb
-		GameRegistry.addShapedRecipe(new ItemStack(ZItems.lightBulb), "GGG", "GOG", "III", 'G', new ItemStack(Block.glass), 'O', new ItemStack(Item.ingotGold), 'I', new ItemStack(Item.ingotIron));
+		GameRegistry.addShapedRecipe(new ItemStack(ZItems.lightBulb), "GGG", "GOG", "III", 'G', new ItemStack(Blocks.glass), 'O', new ItemStack(Items.gold_ingot), 'I', new ItemStack(Items.iron_ingot));
 		
 		// Zinchorium Gem Sword
-		GameRegistry.addShapedRecipe(new ItemStack(ZItems.zinchoriumSword), " Z ", " Z ", " S ", 'Z', new ItemStack(ZItems.greenZinchoriumGem), 'S', new ItemStack(Item.stick));
+		GameRegistry.addShapedRecipe(new ItemStack(ZItems.zinchoriumSword), " Z ", " Z ", " S ", 'Z', new ItemStack(ZItems.greenZinchoriumGem), 'S', new ItemStack(Items.stick));
 		
 		// Zinchorium Gem Pickaxe
-		GameRegistry.addShapedRecipe(new ItemStack(ZItems.zinchoriumPickaxe), "ZZZ", " S ", " S ", 'Z', new ItemStack(ZItems.greenZinchoriumGem), 'S', new ItemStack(Item.stick));
+		GameRegistry.addShapedRecipe(new ItemStack(ZItems.zinchoriumPickaxe), "ZZZ", " S ", " S ", 'Z', new ItemStack(ZItems.greenZinchoriumGem), 'S', new ItemStack(Items.stick));
 		
 		// Zinchorium Gem Axe
-		GameRegistry.addShapedRecipe(new ItemStack(ZItems.zinchoriumAxe), "ZZ ", "ZS ", " S ", 'Z', new ItemStack(ZItems.greenZinchoriumGem), 'S', new ItemStack(Item.stick));
+		GameRegistry.addShapedRecipe(new ItemStack(ZItems.zinchoriumAxe), "ZZ ", "ZS ", " S ", 'Z', new ItemStack(ZItems.greenZinchoriumGem), 'S', new ItemStack(Items.stick));
 		
 		// Zinchorium Gem Shovel
-		GameRegistry.addShapedRecipe(new ItemStack(ZItems.zinchoriumShovel), " Z ", " S ", " S ", 'Z', new ItemStack(ZItems.greenZinchoriumGem), 'S', new ItemStack(Item.stick));
+		GameRegistry.addShapedRecipe(new ItemStack(ZItems.zinchoriumShovel), " Z ", " S ", " S ", 'Z', new ItemStack(ZItems.greenZinchoriumGem), 'S', new ItemStack(Items.stick));
 		
 		// Zinchorium Gem Hoe
-		GameRegistry.addShapedRecipe(new ItemStack(ZItems.zinchoriumHoe), "ZZ ", " S ", " S ", 'Z', new ItemStack(ZItems.greenZinchoriumGem), 'S', new ItemStack(Item.stick));
+		GameRegistry.addShapedRecipe(new ItemStack(ZItems.zinchoriumHoe), "ZZ ", " S ", " S ", 'Z', new ItemStack(ZItems.greenZinchoriumGem), 'S', new ItemStack(Items.stick));
 	
 		// Zinchorium Helmet
 		GameRegistry.addShapedRecipe(new ItemStack(ZItems.zinchoriumHelmet), "ZZZ", "Z Z", "   ", 'Z', new ItemStack(ZItems.greenZinchoriumGem));
@@ -57,9 +57,10 @@ public class ZRecipes {
 	
 	private static void registerFurnaceRecipes() {
 		// Copper ore --> copper ingot
-		GameRegistry.addSmelting(ZBlocks.oreCopper.blockID, new ItemStack(ZItems.ingotCopper, 1), 0.3f);
+		GameRegistry.addSmelting(ZBlocks.oreCopper, new ItemStack(ZItems.ingotCopper, 1), 0.3f);
+		
 		// Zinchorium ore --> Zinchorium Gem (3)
-		GameRegistry.addSmelting(ZBlocks.greenZinchoriumGemOre.blockID, new ItemStack(ZItems.greenZinchoriumGem, 3), 0.5f);
+		GameRegistry.addSmelting(ZBlocks.greenZinchoriumGemOre, new ItemStack(ZItems.greenZinchoriumGem, 3), 0.5f);
 	}
 	
 	public static void registerOreDictThings() {
