20068f322def7f62b1d6acdd4d3daf1f4c8d8620
Shadowfacts <akorde@icloud.com>
2014-12-15 19:14:56 -0500

    Remove old stuff

--- src/main/java/net/shadowfacts/enfusion/config/Configurator.java
+++ src/main/java/net/shadowfacts/enfusion/config/Configurator.java
@@ -8,12 +8,10 @@ public class Configurator {
 	
 	// The all-mighty config
 	private static Configuration config;
-	
-	private static int nextBlockId = 1232;
-	private static int nextItemId = 4288;
+
 	private static int nextEntityId = 1337;
-	
-	
+
+
 	// Config properties
 	// Entity IDs
 	public static int laserBoltEntityId;
@@ -37,22 +35,7 @@ public class Configurator {
 		// Save config
 		config.save();
 	}
-	
-	
-	private static int getBlockId() {
-		int id = nextBlockId;
-		nextBlockId = nextBlockId + 1;
-		
-		return id;
-	}
-	
-	private static int getItemId() {
-		int id = nextItemId;
-		nextItemId = nextItemId + 1;
-		
-		return id;
-	}
-	
+
 	private static int getEntityId() {
 		int id = nextEntityId;
 		nextEntityId = nextEntityId + 1;
