5bcf9cfada6c09fbc5a08a43e64eee955d522f50
Shadowfacts <akorde@icloud.com>
2015-03-02 15:35:26 -0500

    Remove PacketUpdateSolarPanel

--- src/main/java/net/shadowfacts/enfusion/network/EFNetworkManager.java
+++ src/main/java/net/shadowfacts/enfusion/network/EFNetworkManager.java
@@ -1,10 +1,13 @@
 package net.shadowfacts.enfusion.network;
 
+import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.tileentity.TileEntity;
+
+import net.shadowfacts.enfusion.EnFusion;
 
 /**
  * Handles all the network/packet stuff
@@ -16,26 +19,25 @@ public class EFNetworkManager {
 
 	public static void initialize() {
 		// Create the channel
-//		network = NetworkRegistry.INSTANCE.newSimpleChannel(EnFusion.modId);
+		network = NetworkRegistry.INSTANCE.newSimpleChannel(EnFusion.modId);
 
 		// Register all the packets
-//		network.registerMessage(PacketUpdateSolarPanel.class, PacketUpdateSolarPanel.class, 0, Side.CLIENT);
 	}
 
 	public static void sendToServer(IMessage message) {
-//		network.sendToServer(message);
+		network.sendToServer(message);
 	}
 
 	public static void sendToPlayer(IMessage message, EntityPlayerMP player) {
-//		network.sendTo(message, player);
+		network.sendTo(message, player);
 	}
 
 	public static void sendToAllAround(IMessage message, TileEntity te, int range) {
-//		network.sendToAllAround(message, new TargetPoint(te.getWorldObj().provider.dimensionId, te.xCoord, te.yCoord, te.zCoord, range));
+		network.sendToAllAround(message, new NetworkRegistry.TargetPoint(te.getWorldObj().provider.dimensionId, te.xCoord, te.yCoord, te.zCoord, range));
 	}
 
 	public static void sendToAllAround(IMessage message, TileEntity te) {
-//		sendToAllAround(message, te, 64);
+		sendToAllAround(message, te, 64);
 	}
 
 }
