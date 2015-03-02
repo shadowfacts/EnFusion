3acde47a0b684dfa0b061fe883b1f0b18f45eca6
Shadowfacts <akorde@icloud.com>
2015-03-02 18:39:45 -0500

    A whole bunch of recipe shit

--- src/main/java/net/shadowfacts/enfusion/recipes/EFRecipes.java
+++ src/main/java/net/shadowfacts/enfusion/recipes/EFRecipes.java
@@ -21,70 +21,69 @@ public class EFRecipes {
 	
 	private static void registerBlockRecipes() {
 		// Green Zinchorium Light
-		GameRegistry.addShapedRecipe(new ItemStack(EFBlocks.greenZinchoriumLightIdle, 1), "IGI", "GLG", "IGI", 'I', new ItemStack(EFItems.zinchoriumAlloyIngot), 'G', new ItemStack(Blocks.glass), 'L', new ItemStack(EFItems.lightBulb, 1));
+		GameRegistry.addShapedRecipe(new ItemStack(EFBlocks.greenZinchoriumLightIdle, 1), "IGI", "GLG", "IGI", 'I', EFItems.zinchoriumAlloyIngot, 'G', Blocks.glass, 'L', EFItems.lightBulb);
 
 		// Zinchorium Block
-		GameRegistry.addShapedRecipe(new ItemStack(EFBlocks.blockZinchorium), "ZZZ", "ZZZ", "ZZZ", 'Z', new ItemStack(EFItems.zinchoriumAlloyIngot));
+		GameRegistry.addShapedRecipe(new ItemStack(EFBlocks.blockZinchorium), "ZZZ", "ZZZ", "ZZZ", 'Z', EFItems.zinchoriumAlloyIngot);
 
 		// Solar Panel Tier 1
-		GameRegistry.addShapedRecipe(new ItemStack(EFBlocks.solarPanelTier1), "MMM", "CFC", "CCC", 'M', new ItemStack(EFItems.mirror), 'C', new ItemStack(Blocks.cobblestone), 'F', new ItemStack(EFItems.basicCapacitor));
+		GameRegistry.addShapedRecipe(new ItemStack(EFBlocks.solarPanelTier1), "MMM", "CFC", "CCC", 'M', EFItems.mirror, 'C', Blocks.cobblestone, 'F', EFItems.basicCapacitor);
 
 		// Solar Panel Tier 2
-		GameRegistry.addShapelessRecipe(new ItemStack(EFBlocks.solarPanelTier2), new ItemStack(EFBlocks.solarPanelTier1), new ItemStack(EFItems.enhancedCapacitor), new ItemStack(EFBlocks.solarPanelTier1));
+		GameRegistry.addShapelessRecipe(new ItemStack(EFBlocks.solarPanelTier2), EFBlocks.solarPanelTier1, EFItems.enhancedCapacitor, EFBlocks.solarPanelTier1);
 
 		// Solar Panel Tier 3
-		GameRegistry.addShapelessRecipe(new ItemStack(EFBlocks.solarPanelTier3), new ItemStack(EFBlocks.solarPanelTier2), new ItemStack(EFItems.energeticCapacitor), new ItemStack(EFBlocks.solarPanelTier1));
+		GameRegistry.addShapelessRecipe(new ItemStack(EFBlocks.solarPanelTier3), EFBlocks.solarPanelTier2, EFItems.energeticCapacitor, EFBlocks.solarPanelTier1);
 
 		// Solar Panel Tier 4
-		GameRegistry.addShapelessRecipe(new ItemStack(EFBlocks.solarPanelTier4), new ItemStack(EFBlocks.solarPanelTier3), new ItemStack(EFItems.hyperEnergeticCapacitor), new ItemStack(EFBlocks.solarPanelTier3));
+		GameRegistry.addShapelessRecipe(new ItemStack(EFBlocks.solarPanelTier4), EFBlocks.solarPanelTier3, EFItems.hyperEnergeticCapacitor, EFBlocks.solarPanelTier3);
 
 		// Zinchorium Furnace
-		GameRegistry.addShapedRecipe(new ItemStack(EFBlocks.zinchoriumFurnace), "ZZZ", "ZFZ", "ZZZ", 'Z', new ItemStack(EFItems.zinchoriumAlloyIngot), 'F', new ItemStack(Blocks.furnace));
+		GameRegistry.addShapedRecipe(new ItemStack(EFBlocks.zinchoriumFurnace), "ZZZ", "ZFZ", "ZZZ", 'Z', EFItems.zinchoriumAlloyIngot, 'F', Blocks.furnace);
 	}
 	
 	private static void registerItemRecipes() {
 		// Light bulb
-		GameRegistry.addShapedRecipe(new ItemStack(EFItems.lightBulb), "GGG", "GOG", "III", 'G', new ItemStack(Blocks.glass), 'O', new ItemStack(Items.gold_ingot), 'I', new ItemStack(Items.iron_ingot));
+		GameRegistry.addShapedRecipe(new ItemStack(EFItems.lightBulb), "GGG", "GOG", "III", 'G', Blocks.glass, 'O', Items.gold_ingot, 'I', Items.iron_ingot);
 		
 		// Zinchorium Gem Sword
-		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumSword), "Z", "Z", "S", 'Z', new ItemStack(EFItems.zinchoriumAlloyIngot), 'S', new ItemStack(Items.stick));
+		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumSword), "Z", "Z", "S", 'Z', EFItems.zinchoriumAlloyIngot, 'S', Items.stick);
 		
 		// Zinchorium Gem Pickaxe
-		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumPickaxe), "ZZZ", " S ", " S ", 'Z', new ItemStack(EFItems.zinchoriumAlloyIngot), 'S', new ItemStack(Items.stick));
+		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumPickaxe), "ZZZ", " S ", " S ", 'Z', EFItems.zinchoriumAlloyIngot, 'S', Items.stick);
 		
 		// Zinchorium Gem Axe
-		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumAxe), "ZZ", "ZS", " S", 'Z', new ItemStack(EFItems.zinchoriumAlloyIngot), 'S', new ItemStack(Items.stick));
+		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumAxe), "ZZ", "ZS", " S", 'Z', EFItems.zinchoriumAlloyIngot, 'S', Items.stick);
 		
 		// Zinchorium Gem Shovel
-		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumShovel), " Z ", " S ", " S ", 'Z', new ItemStack(EFItems.zinchoriumAlloyIngot), 'S', new ItemStack(Items.stick));
+		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumShovel), "Z", "S", "S", 'Z', EFItems.zinchoriumAlloyIngot, 'S', Items.stick);
 
 //		Zinchorium Paxel
 		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumPaxel), "AQP", " S ", " S ", 'A', EFItems.zinchoriumAxe, 'Q', EFItems.zinchoriumShovel, 'P', EFItems.zinchoriumPickaxe, 'S', Items.stick);
 
 		// Zinchorium Gem Hoe
-		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumHoe), "ZZ", " S", " S", 'Z', new ItemStack(EFItems.zinchoriumAlloyIngot), 'S', new ItemStack(Items.stick));
+		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumHoe), "ZZ", " S", " S", 'Z', EFItems.zinchoriumAlloyIngot, 'S', Items.stick);
 	
 		// Zinchorium Helmet
-		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumHelmet), "ZZZ", "Z Z", "   ", 'Z', new ItemStack(EFItems.zinchoriumAlloyIngot));
+		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumHelmet), "ZZZ", "Z Z", "   ", 'Z', EFItems.zinchoriumAlloyIngot);
 		
 		// Zinchorium Chestplate
-		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumChestplate), "Z Z", "ZZZ", "ZZZ", 'Z', new ItemStack(EFItems.zinchoriumAlloyIngot));
+		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumChestplate), "Z Z", "ZZZ", "ZZZ", 'Z', EFItems.zinchoriumAlloyIngot);
 		
 		// Zinchoruim Leggings
-		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumLeggings), "ZZZ", "Z Z", "Z Z", 'Z', new ItemStack(EFItems.zinchoriumAlloyIngot));
+		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumLeggings), "ZZZ", "Z Z", "Z Z", 'Z', EFItems.zinchoriumAlloyIngot);
 		
 		// Zinchorium Boots
-		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumBoots), "Z Z", "Z Z", "   ", 'Z', new ItemStack(EFItems.zinchoriumAlloyIngot));
-		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumBoots), "   ", "Z Z", "Z Z", 'Z', new ItemStack(EFItems.zinchoriumAlloyIngot));
+		GameRegistry.addShapedRecipe(new ItemStack(EFItems.zinchoriumBoots), "Z Z", "Z Z", 'Z', EFItems.zinchoriumAlloyIngot);
 
 		// Zinchorium Block -> Zinchorium Alloy Ingot
-		GameRegistry.addShapelessRecipe(new ItemStack(EFItems.zinchoriumAlloyIngot, 9), new ItemStack(EFBlocks.blockZinchorium));
+		GameRegistry.addShapelessRecipe(new ItemStack(EFItems.zinchoriumAlloyIngot, 9), EFBlocks.blockZinchorium);
 
 		// Mirror
-		GameRegistry.addShapedRecipe(new ItemStack(EFItems.mirror, 3), "GGG", "III", "   ", 'G', new ItemStack(Blocks.glass), 'I', new ItemStack(Items.iron_ingot));
+		GameRegistry.addShapedRecipe(new ItemStack(EFItems.mirror, 3), "GGG", "III", "   ", 'G', Blocks.glass, 'I', Items.iron_ingot);
 
 		// Basic Capacitor
-		GameRegistry.addShapedRecipe(new ItemStack(EFItems.basicCapacitor), "  R", " G ", "R  ", 'R', new ItemStack(Items.redstone), 'G', Items.gold_nugget);
+		GameRegistry.addShapedRecipe(new ItemStack(EFItems.basicCapacitor), "  R", " G ", "R  ", 'R', Items.redstone, 'G', Items.gold_nugget);
 
 		// Enhanced Capacitor
 		GameRegistry.addShapedRecipe(new ItemStack(EFItems.enhancedCapacitor), "  B", " R ", "B  ", 'B', EFItems.basicCapacitor, 'R', Items.redstone);
