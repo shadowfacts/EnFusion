45865827c6ce47129ae6b9d4fd20b5bfa20409d2
Shadowfacts <akorde@icloud.com>
2015-03-15 11:38:23 -0400

    Add JavaDoc to EnergyUtils

--- src/main/java/net/shadowfacts/enfusion/energy/EnergyUtils.java
+++ src/main/java/net/shadowfacts/enfusion/energy/EnergyUtils.java
@@ -10,12 +10,23 @@ public class EnergyUtils {
 
 	public static final String TAG_ENERGY = "Energy";
 
+	/**
+	 * Sets the energy in an {@link net.minecraft.item.ItemStack}
+	 * @param stack The {@link net.minecraft.item.ItemStack}
+	 * @param energy How much energy
+	 * @return The modified {@link net.minecraft.item.ItemStack}
+	 */
 	public static ItemStack setEnergyInStack(ItemStack stack, int energy) {
 		if (stack.stackTagCompound == null) stack.stackTagCompound = new NBTTagCompound();
 		stack.stackTagCompound.setInteger(TAG_ENERGY, energy);
 		return stack;
 	}
 
+	/**
+	 * Gets the amount of energy in an {@link ItemStack}, returns 0 if if {@link net.minecraft.nbt.NBTTagCompound} is null or doesn't have the "Energy" key.
+	 * @param stack The {@link net.minecraft.item.ItemStack}
+	 * @return The amount of energy.
+	 */
 	public static int getEnergyInStack(ItemStack stack) {
 		if (stack.stackTagCompound == null || !stack.stackTagCompound.hasKey(TAG_ENERGY)) {
 			return 0;
