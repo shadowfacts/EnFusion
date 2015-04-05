d63ab00a4d473b28ffb16ff02269cbf0a4909006
Shadowfacts <akorde@icloud.com>
2015-04-05 10:39:24 -0400

    de-pluralize things

--- src/main/java/net/shadowfacts/enfusion/config/EFConfig.java
+++ src/main/java/net/shadowfacts/enfusion/config/EFConfig.java
@@ -48,24 +48,24 @@ public class EFConfig {
 
 //	Tools
 //	Laser Pistol
-	@ConfigProperty(category = "items.tools.laserPistol", comment = "Maximum RF that can be stored.")
+	@ConfigProperty(category = "item.tool.laserPistol", comment = "Maximum RF that can be stored.")
 	public static int laserPistolCapacity = 32000;
-	@ConfigProperty(category = "items.tools.laserPistol", comment = "Maximum RF that the laser pistol can receive per tick.")
+	@ConfigProperty(category = "item.tool.laserPistol", comment = "Maximum RF that the laser pistol can receive per tick.")
 	public static int laserPistolMaxReceive = 64;
-	@ConfigProperty(category = "items.tools.laserPistol")
+	@ConfigProperty(category = "item.tool.laserPistol")
 	public static int laserPistolEnergyPerUse = 16;
 //	Mining Laser
-	@ConfigProperty(category = "items.tools.miningLaser", comment = "Maximum RF that can be stored.")
+	@ConfigProperty(category = "item.tool.miningLaser", comment = "Maximum RF that can be stored.")
 	public static int miningLaserCapacity = 32000;
-	@ConfigProperty(category = "items.tools.miningLaser", comment = "Maximum RF that the mining laser can receive per tick.")
+	@ConfigProperty(category = "item.tool.miningLaser", comment = "Maximum RF that the mining laser can receive per tick.")
 	public static int miningLaserMaxReceive= 64;
-	@ConfigProperty(category = "items.tools.miningLaser")
+	@ConfigProperty(category = "item.tool.miningLaser")
 	public static int miningLaserEnergyPerUse = 16;
 //	Leaf Blower
-	@ConfigProperty(category = "items.tools.leafBlower", comment = "Maximum RF that can be stored.")
+	@ConfigProperty(category = "item.tool.leafBlower", comment = "Maximum RF that can be stored.")
 	public static int leafBlowerCapacity = 16000;
-	@ConfigProperty(category = "items.tools.leafBlower", comment = "Maximum RF that the leaf blower can receive per tick.")
+	@ConfigProperty(category = "item.tool.leafBlower", comment = "Maximum RF that the leaf blower can receive per tick.")
 	public static int leafBlowerMaxReceive = 32;
-	@ConfigProperty(category = "items.tools.leafBlower")
+	@ConfigProperty(category = "item.tool.leafBlower")
 	public static int leafBlowerEnergyPerUse = 16;
 }
