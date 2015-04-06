6dd0aea084dd1f222e4c414915e772d0d5701978
Shadowfacts <akorde@icloud.com>
2015-04-05 21:36:57 -0400

    Meh
    Fix #32

--- src/main/java/net/shadowfacts/enfusion/block/misc/BlockConcrete.java
+++ src/main/java/net/shadowfacts/enfusion/block/misc/BlockConcrete.java
@@ -3,6 +3,7 @@ package net.shadowfacts.enfusion.block.misc;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.world.World;
+
 import net.shadowfacts.enfusion.block.EFBlock;
 
 /**
@@ -13,14 +14,13 @@ public class BlockConcrete extends EFBlock {
 
 	public BlockConcrete() {
 		super(Material.rock);
+//		this.slipperiness = 1f;
 	}
 
 	@Override
 	public void onEntityWalking(World world, int x, int y, int z, Entity entity) {
-		entity.motionX *= 2;
-		entity.motionZ *= 3;
-//		entity.stepHeight = 1f;
-//		TODO: change step height back to 0.5f after someone stops walking on the concrete
+		entity.motionX *= 1.7f;
+		entity.motionZ *= 1.7f;
 	}
 
 }
