6b781b02ac70ed1bd496be5d36ef486311d26c46
Shadowfacts <me@shadowfacts.net>
2015-05-30 14:24:35 -0400

    Nuke repo

--- src/main/java/net/shadowfacts/enfusion/client/gui/GUIs.java
+++
@@ -1,5 +0,0 @@
-package net.shadowfacts.enfusion.client.gui;
-
-public class GUIs {
-	public static final int ZINCHORIUM_FURNACE = 1;
-}
--- src/main/java/net/shadowfacts/enfusion/machine/zinchoriumchest/TileEntityZinchoriumChest.java
+++
@@ -1,9 +0,0 @@
-package net.shadowfacts.enfusion.machine.zinchoriumchest;
-
-import net.minecraft.tileentity.TileEntity;
-
-/**
- * @author shadowfacts
- */
-public class TileEntityZinchoriumChest extends TileEntity {
-}
