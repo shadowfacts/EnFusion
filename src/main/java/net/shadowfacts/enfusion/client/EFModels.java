069ed0811d111480fc97e625e6ba8c3c8b3d8040
Shadowfacts <me@shadowfacts.net>
2015-06-07 17:30:18 -0400

    Models

---
+++ src/main/java/net/shadowfacts/enfusion/client/EFModels.java
@@ -0,0 +1,17 @@
+package net.shadowfacts.enfusion.client;
+
+import net.minecraft.client.renderer.entity.RenderManager;
+import nova.core.render.model.ModelProvider;
+
+/**
+ * @author shadowfacts
+ */
+public class EFModels {
+
+	public static ModelProvider crankModel;
+
+	public static void registerModels(RenderManager renderManager) {
+
+	}
+
+}
