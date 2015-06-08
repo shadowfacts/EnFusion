331e33be3d7e4ea7ec8f5f3263e1246504be5835
Shadowfacts <me@shadowfacts.net>
2015-06-08 08:44:19 -0400

    Register dusts

--- src/main/java/net/shadowfacts/enfusion/item/EFItems.java
+++ src/main/java/net/shadowfacts/enfusion/item/EFItems.java
@@ -1,5 +1,6 @@
 package net.shadowfacts.enfusion.item;
 
+import net.shadowfacts.enfusion.item.dust.*;
 import net.shadowfacts.enfusion.item.ingot.*;
 import nova.core.item.ItemFactory;
 import nova.core.item.ItemManager;
@@ -14,11 +15,21 @@ public class EFItems {
 	public static ItemFactory ingotLead;
 	public static ItemFactory ingotSilver;
 
+//	Dusts
+	public static ItemFactory dustCopper;
+	public static ItemFactory dustLead;
+	public static ItemFactory dustSilver;
+
 	public static void registerItems(ItemManager itemManager) {
 //		Ingots
 		ingotCopper = itemManager.register(IngotCopper.class);
 		ingotLead = itemManager.register(IngotLead.class);
 		ingotSilver = itemManager.register(IngotSilver.class);
+
+//		Dusts
+		dustCopper = itemManager.register(DustCopper.class);
+		dustLead = itemManager.register(DustLead.class);
+		dustSilver = itemManager.register(DustSilver.class);
 	}
 
 }
