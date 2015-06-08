292f2363ca06306a59b170c165ada6d6aaf4896a
Shadowfacts <me@shadowfacts.net>
2015-06-08 18:44:52 -0400

    Oops, forgot sulfur texture

--- src/main/java/net/shadowfacts/enfusion/client/EFItemTextures.java
+++ src/main/java/net/shadowfacts/enfusion/client/EFItemTextures.java
@@ -19,15 +19,23 @@ public class EFItemTextures {
 	public static ItemTexture dustCopper;
 	public static ItemTexture dustIron;
 	public static ItemTexture dustSilver;
+
+//	Misc
+	public static ItemTexture sulfur;
+
 	public static void registerTextures(RenderManager renderManager) {
 //		Ingots
 		ingotCopper = renderManager.registerTexture(new ItemTexture(EnFusion.id, "missing"));
 		ingotLead = renderManager.registerTexture(new ItemTexture(EnFusion.id, "missing"));
 		ingotSilver = renderManager.registerTexture(new ItemTexture(EnFusion.id, "missing"));
+
 //		Dusts
 		dustCopper = renderManager.registerTexture(new ItemTexture(EnFusion.id, "missing"));
 		dustLead = renderManager.registerTexture(new ItemTexture(EnFusion.id, "missing"));
 		dustSilver = renderManager.registerTexture(new ItemTexture(EnFusion.id, "missing"));
+
+//		Misc
+		sulfur = renderManager.registerTexture(new ItemTexture(EnFusion.id, "missing"));
 	}
 
 }
