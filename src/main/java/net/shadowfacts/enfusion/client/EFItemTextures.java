349eb22eacbeb2dd7349d46c1c665f6e5ea5ffc5
Shadowfacts <me@shadowfacts.net>
2015-06-08 18:44:32 -0400

    Add dust textures

--- src/main/java/net/shadowfacts/enfusion/client/EFItemTextures.java
+++ src/main/java/net/shadowfacts/enfusion/client/EFItemTextures.java
@@ -15,10 +15,19 @@ public class EFItemTextures {
 	public static ItemTexture ingotLead;
 	public static ItemTexture ingotSilver;
 
+//	Dusts
+	public static ItemTexture dustCopper;
+	public static ItemTexture dustIron;
+	public static ItemTexture dustSilver;
 	public static void registerTextures(RenderManager renderManager) {
+//		Ingots
 		ingotCopper = renderManager.registerTexture(new ItemTexture(EnFusion.id, "missing"));
 		ingotLead = renderManager.registerTexture(new ItemTexture(EnFusion.id, "missing"));
 		ingotSilver = renderManager.registerTexture(new ItemTexture(EnFusion.id, "missing"));
+//		Dusts
+		dustCopper = renderManager.registerTexture(new ItemTexture(EnFusion.id, "missing"));
+		dustLead = renderManager.registerTexture(new ItemTexture(EnFusion.id, "missing"));
+		dustSilver = renderManager.registerTexture(new ItemTexture(EnFusion.id, "missing"));
 	}
 
 }
--- src/main/java/net/shadowfacts/enfusion/item/dust/DustSilver.java
+++ src/main/java/net/shadowfacts/enfusion/item/dust/DustSilver.java
@@ -1,8 +1,12 @@
 package net.shadowfacts.enfusion.item.dust;
 
 import net.shadowfacts.enfusion.EnFusion;
+import net.shadowfacts.enfusion.client.EFItemTextures;
 import nova.core.component.Category;
 import nova.core.item.Item;
+import nova.core.render.texture.ItemTexture;
+
+import java.util.Optional;
 
 /**
  * @author shadowfacts
@@ -11,6 +15,11 @@ public class DustSilver extends Item {
 
 	public DustSilver() {
 		add(new Category(EnFusion.id + ".category.resources"));
+	}
+
+	@Override
+	public Optional<ItemTexture> getTexture() {
+		return Optional.of(EFItemTextures.dustSilver);
 	}
 
 	@Override
