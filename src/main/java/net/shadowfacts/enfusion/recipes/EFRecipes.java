781a4cde02066331ba4a17c1c6a6ad575585bd59
Shadowfacts <me@shadowfacts.net>
2015-07-13 19:39:31 -0400

    Update EFRecipes for new GrinderRecipe

--- src/main/java/net/shadowfacts/enfusion/recipes/EFRecipes.java
+++ src/main/java/net/shadowfacts/enfusion/recipes/EFRecipes.java
@@ -2,9 +2,11 @@ package net.shadowfacts.enfusion.recipes;
 
 import net.shadowfacts.enfusion.block.EFBlocks;
 import net.shadowfacts.enfusion.item.EFItems;
-import net.shadowfacts.enfusion.machine.grinder.recipe.GrinderOutput;
-import net.shadowfacts.enfusion.machine.grinder.recipe.GrinderRecipe;
+import net.shadowfacts.enfusion.api.recipe.grinder.GrinderOutput;
+import net.shadowfacts.enfusion.api.recipe.grinder.GrinderRecipe;
 import nova.core.recipes.RecipeManager;
+import nova.core.recipes.crafting.OreItemIngredient;
+import nova.core.recipes.crafting.SpecificItemIngredient;
 
 import java.util.ArrayList;
 
@@ -34,24 +36,24 @@ public class EFRecipes {
 		outputs.add(new GrinderOutput(EFItems.dustCopper, 0.25));
 		outputs.add(new GrinderOutput(EFItems.dustIron, 0.25));
 		outputs.add(new GrinderOutput(EFItems.sulfur, 0.5));
-		recipeManager.addRecipe(new GrinderRecipe(EFBlocks.oreChalcopyrite, outputs));
+		recipeManager.addRecipe(new GrinderRecipe(new SpecificItemIngredient("enfusion.ore.chalcopyrite"), outputs, "enfusion.ore.chalcopyrite"));
 		outputs = new ArrayList<>();
 
 //		Galena
 		outputs.add(new GrinderOutput(EFItems.dustLead, 0.5));
 		outputs.add(new GrinderOutput(EFItems.sulfur, 0.5));
-		recipeManager.addRecipe(new GrinderRecipe(EFBlocks.oreGalena, outputs));
+		recipeManager.addRecipe(new GrinderRecipe(new SpecificItemIngredient("enfusion.ore.galena"), outputs, "enfusion.ore.galena"));
 		outputs = new ArrayList<>();
 
 //		Chlorargyrite
 		outputs.add(new GrinderOutput(EFItems.dustSilver, 1));
-		recipeManager.addRecipe(new GrinderRecipe(EFBlocks.oreChlorargyrite, outputs));
+		recipeManager.addRecipe(new GrinderRecipe(new SpecificItemIngredient("enfusion.ore.chlorargyrite"), outputs, "enfusion.ore.chlorargyrite"));
 		outputs = new ArrayList<>();
 
 //		Pyrargyrite
 		outputs.add(new GrinderOutput(EFItems.dustSilver, 0.5));
 		outputs.add(new GrinderOutput(EFItems.sulfur, 0.5));
-		recipeManager.addRecipe(new GrinderRecipe(EFBlocks.orePyrargyrite, outputs));
+		recipeManager.addRecipe(new GrinderRecipe(new SpecificItemIngredient("enfusion.ore.pyrargyrite"), outputs, "enfusion.ore.pyrargyrite"));
 		outputs = new ArrayList<>();
 
 	}
